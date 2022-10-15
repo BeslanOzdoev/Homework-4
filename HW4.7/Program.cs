b7a6f22a1be002a3f1e24e12d0fd73f8934ba60a
Beslan <biozdoev@gmail.com>
2022-10-16 00:07:13 +0300

    Seventh task

---
+++ HW4.7/Program.cs
@@ -0,0 +1,29 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+            int n = 12;
+            int[] arr = new int[n];
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            
+            Random rnd = new Random();
+ 
+            for (int i = 0; i < n; i++)
+            {
+            {
+                arr[i] = rnd.Next();
+                Console.Write("{0} ", arr[i]);
+            }
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            int delta = max - min;
+
+Console.WriteLine();
+Console.WriteLine("Максимальное значение: {0}, минимальное значение: {1}, разница между максимальным и минимальным значением: {2}", max, min, delta);
