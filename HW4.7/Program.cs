fc03b2d30de036298e9fc95e5991d9fbaba7d008
Beslan <biozdoev@gmail.com>
2022-10-30 14:14:08 +0300

    Seventh Task

--- HW4.7/Program.cs
+++ HW4.7/Program.cs
@@ -1,29 +1,29 @@
 ﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
-
-            int n = 12;
-            int[] arr = new int[n];
-            int max = int.MinValue;
-            int min = int.MaxValue;
-            
-            Random rnd = new Random();
- 
-            for (int i = 0; i < n; i++)
-            {
-            {
-                arr[i] = rnd.Next();
-                Console.Write("{0} ", arr[i]);
-            }
-                if (arr[i] > max)
-                {
-                    max = arr[i];
-                }
-                if (arr[i] < min)
-                {
-                    min = arr[i];
-                }
-            }
-
-            int delta = max - min;
-
-Console.WriteLine();
-Console.WriteLine("Максимальное значение: {0}, минимальное значение: {1}, разница между максимальным и минимальным значением: {2}", max, min, delta);
+void Pechat_Massiva(double[] massiv)
+{
+    foreach (var item in massiv)
+    {
+        Console.Write($"{item} ");
+    }
+}
+double Raznica_max_min(double[] massiv)
+{
+    double max = massiv[0] , min= massiv[0], raznica = 0;
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        if (massiv[i] > max)
+        {
+            max = massiv[i];
+        }
+        if (massiv[i] < min)
+        {
+            min = massiv[i];
+        }
+        raznica = max - min;
+    }
+    return raznica;
+}
+double[] Array = {0.158, 0.85, 8.468, 4.586, 99.85, 0.05};
+Pechat_Massiva(Array);
+Console.WriteLine(" ");
+Console.Write($"Разница между максимальным и минимальным числом {Raznica_max_min(Array)}");
