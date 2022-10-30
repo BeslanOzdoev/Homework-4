5ac8546f66c6b60a3ea3fdafcd44eb3b48de39cb
Beslan <biozdoev@gmail.com>
2022-10-30 14:05:25 +0300

    Fifth Task

--- HW4.5/Program.cs
+++ HW4.5/Program.cs
@@ -1,20 +1,36 @@
 ﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
-
-int n = 25;
-            int[] arr = new int[n];
-            int k = 0, sum ;
-            Random rnd = new Random();
- 
-            for (int i = 0; i < n; i++)
-            {
-                arr[i] = rnd.Next(0, 99);
-                Console.Write("{0} ", arr[i]);
-            }
-
-            var even = arr.Where((element, index) => index % 2 != 0);
-            var odd = arr.Where((element, index) => index % 2 == 0);
-
-            Console.WriteLine();
-            Console.WriteLine("Чётные. Количество: {0}. Сумма: {1}.", even.Count(), even.Sum());
-            Console.WriteLine();
-            Console.WriteLine("Нечётные. Количество: {0}. Сумма: {1}.", odd.Count(), odd.Sum());
+void Zapolnenie_Massiva(int[] massiv, int min, int max)
+{
+    Random Znach = new Random();
+    for (int ind = 0; ind < massiv.Length; ind++)
+    {
+        massiv[ind] = Znach.Next(min, max);
+    }
+}
+void Pechat_Massiva(int[] massiv)
+{
+    foreach (var item in massiv)
+    {
+        Console.Write($"{item} ");
+    }
+}
+int Summa_chisel(int[] massiv)
+{
+    int summa = 0;
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        if (i % 2 == 1) summa += massiv[i];
+    }
+    return summa;
+}
+Console.Write("Введите минимальное число для значений массива:");
+int Min = int.Parse(Console.ReadLine()??"0");
+Console.Write("Введите максимальное число для значений массива:");
+int Max = int.Parse(Console.ReadLine()??"0");
+Console.Write("Введите длинну массива:");
+int dlinna = int.Parse(Console.ReadLine() ?? "0");
+int[] Array = new int[dlinna];
+Zapolnenie_Massiva(max: Max+1, min: Min, massiv: Array);
+Pechat_Massiva(Array);
+Console.WriteLine(" ");
+Console.WriteLine($"Сумма чисел на нечётной позиции: {Summa_chisel(Array)}");
