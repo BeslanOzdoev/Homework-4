4e0c5afa690edf1215c1dd8d329b4d10c217b62e
Beslan <biozdoev@gmail.com>
2022-10-30 14:02:17 +0300

    Third Task

--- HW4.3/Program.cs
+++ HW4.3/Program.cs
@@ -1,25 +1,43 @@
 ﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
-
-            int n = 12;
-            int[] arr = new int[n];
-            int k = 0, l = 0;
-            Random rnd = new Random();
- 
-            for (int i = 0; i < n; i++)
-            {
-                arr[i] = rnd.Next(100, 999);
-                Console.Write("{0} ", arr[i]);
-            }
-
-            foreach(int element in arr)
-            {
-                if (element % 2 == 0)
-                {
-                    k++;
-                    l = n - k;
-                }   
-
-            }
-            Console.WriteLine();
-            Console.WriteLine("Количество четных элементов = {0}, Количество нечетных элементов = {1}", k, l);
-
+void Zapolnenie_Massiva(int[] massiv)
+{
+    Random Znach = new Random();
+    for (int ind = 0; ind < massiv.Length; ind++)
+    {
+        massiv[ind] = Znach.Next(0, 1000);
+    }
+}
+void Pechat_Massiva(int[] massiv)
+{
+    foreach (var item in massiv)
+    {
+        Console.Write($"{item} ");
+    }
+}
+int Kolichestvo_chisel(int[] massiv, int proverka_chet)
+{
+    int kolichestvo = 0;
+    if (proverka_chet == 1)
+    {
+        for(int i =0; i<massiv.Length;i++)
+        {
+            if (massiv[i]%2==1) kolichestvo++;
+        }        
+    }
+    else if (proverka_chet == 0)
+    {
+        for(int i =0; i<massiv.Length;i++)
+        {
+            if (massiv[i]%2==0) kolichestvo++;
+        } 
+    }
+    return kolichestvo;
+}
+Console.Write("Введите длинну массива:");
+int dlinna = int.Parse(Console.ReadLine() ?? "0");
+int[] Array = new int[dlinna];
+Zapolnenie_Massiva(Array);
+Pechat_Massiva(Array);
+Console.WriteLine(" ");
+Console.WriteLine($"Количество четных элементов {Kolichestvo_chisel(Array, 0)}");
+Console.WriteLine($"Количество нечетных элементов {Kolichestvo_chisel(Array, 1)}");
